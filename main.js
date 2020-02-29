176b6ec14327618540667d0c11112ae9a74d836b
ToXIc-Dev <4998146+ToXIc-Dev@users.noreply.github.com>
2020-02-29 16:10:54 +0000

    Added presets to interface and tray shortcut
    Scan for a new bulb and click set as tray shortcut bulb to actiavte the tray shortcuts

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const electron = require("electron");
-var colorsys = require('colorsys')
-const TPLSmartDevice = require('tplink-lightbulb')
-var express = require('express');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+var colorsys = require('colorsys')
+const {Client} = require('tplink-smarthome-api');
+const client = new Client();
+var express = require('express');
+const storage = require('electron-json-storage');
 const Menu = electron.Menu
 const path = require('path')
 const url = require('url')
 const Tray = electron.Tray
-const ipc = electron.ipcMain;
 const dialog = require('electron').dialog;
 var appex = express();
-
-var interport = 8080; // Enter the port to run the API on.
+appex.use(express.static(__dirname + '/public'));
+var interport = 8081; // Enter the port to run the API on.
 
 const iconPath = path.join(__dirname, 'smart-bulb.ico')
 let tray = null
@@ -24,17 +25,20 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 900,
+    width: 900,
+    height: 420,
+	autoHideMenuBar: true,
     webPreferences: {
       //preload: path.join(__dirname, 'preload.js'),
-      //nodeIntegration: true
+      //nodeIntegration: true,
+	  webSecurity: false,
+      allowDisplayingInsecureContent: true
     }
   })
 
   // and load the index.html of the app.
-  //mainWindow.loadFile('index.html')
-  mainWindow.loadURL('http://localhost:8080/')
+  //mainWindow.loadFile(__dirname + '/public/index.html')
+  mainWindow.loadURL('http://localhost:8081/')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -58,17 +62,37 @@ app.on('ready', createWindow)
 
 app.on('ready', function(){
   tray = new Tray(iconPath)
-
-  let template = [
+  
+  const contextMenu = Menu.buildFromTemplate([
+    {label: "Bulb On", click: (item, window, event) => {
+      bulbCMD("on");
+    }},
+	{label: "Bulb Off", click: (item, window, event) => {
+      bulbCMD("off");
+    }},
+	{ type: 'separator' },
+	{label: "Preset 1", click: (item, window, event) => {
+      bulbCMD("preset", 0);
+    }},
+	{label: "Preset 2", click: (item, window, event) => {
+      bulbCMD("preset", 1);
+    }},
+	{label: "Preset 3", click: (item, window, event) => {
+      bulbCMD("preset", 2);
+    }},
+	{label: "Preset 4", click: (item, window, event) => {
+      bulbCMD("preset", 3);
+    }},
+	
+	{ type: 'separator' },
     {label: "Open", click: (item, window, event) => {
-      showWindow();
+      showMWindow();
     }},
     {label: "Exit", click: (item, window, event) => {
       process.exit();
     }}
-  ]
+  ])
 
-  const contextMenu = Menu.buildFromTemplate(template)
   tray.setContextMenu(contextMenu)
   tray.setToolTip('TP Link Smart Bulb Control')
   tray.on('click', toggleWindow)
@@ -93,241 +117,325 @@ const toggleWindow = () => {
   if (mainWindow.isVisible()) {
     mainWindow.hide()
   } else {
-    showWindow()
+    showMWindow()
   }
 }
 
-const showWindow = () => {
-  //const position = getWindowPosition()
-  //mainWindow.setPosition(position.x, position.y, false)
+const showMWindow = () => {
   mainWindow.show()
   mainWindow.focus()
 }
 
-appex.use(express.static(__dirname + '/public')); 
-//redirect / to our index.html file
-appex.get('/', function(req, res,next) {  
+function bulbCMD(cmd, scmd) {
+	var bipp
+	storage.get('bulb', function(error, data) {
+		if (error) throw error;
+		console.log(data);
+		bipp = data;
+	if (cmd == "on"){
+		console.log("cmd: on");
+		client.getDevice({host: bipp}).then(device => {
+			device.lighting.setLightState({
+				on_off: true,
+			});
+		});
+	} else if (cmd == "off"){
+		console.log("cmd: off");
+		client.getDevice({host: bipp}).then(device => {
+			device.lighting.setLightState({
+				on_off: false,
+			});
+		});
+	} else if (cmd == "preset"){
+		console.log("Preset: " + scmd);
+		client.getDevice({ host: bipp }).then(device => {
+			device.getSysInfo().then(info => {
+				
+				var bright = info["preferred_state"][scmd]["brightness"];
+				
+				if (info["preferred_state"][scmd]["color_temp"] > 0) {
+					
+					client.getDevice({ host: bipp }).then(device => {
+						device.lighting.setLightState({
+							on_off: true,
+                               brightness: bright,
+							hue: 0,
+                               saturation: 0,
+                               color_temp: info["preferred_state"][scmd]["color_temp"]
+					});
+					});
+					
+				} else if (info["preferred_state"][scmd]["hue"] > 0 || info["preferred_state"][scmd]["saturation"] > 0) {
+					
+					client.getDevice({ host: bipp }).then(device => {
+						device.lighting.setLightState({
+							on_off: true,
+                               brightness: bright,
+                               hue: info["preferred_state"][scmd]["hue"],
+                               saturation: info["preferred_state"][scmd]["saturation"],
+							color_temp: 0
+					});
+					});
+					
+				}
+               })
+		});
+	}
+	});
+}
+
+appex.get('/', function(req, res,next) { 
     res.sendFile(__dirname + '/public/index.html');
 });
 
+appex.get('/cfgs', function(req, res,next) {
+	if (req.query.bulb) {
+		var bulb = req.query.bulb
+		storage.set('bulb', bulb, function(error) {
+				if (error) throw error;
+		});
+	}
+	res.send('Data Received')
+});
 
-appex.get('/api', function(req, res){
-  var cmd = req.query.cmd;
+
+appex.get('/api', function(req, res) {
+    var cmd = req.query.cmd;
+
+    console.log('');
+    console.log("Command Given: " + cmd);
+		
+	
+    if (cmd == "scan") {
+
+        var array = [];
+		
+        client.startDiscovery({ discoveryTimeout: 250 });
+		
+		client.on('device-online', (device) => {
+            var feed = {
+                name: device.alias,
+                ip: device.host
+            };
+            array.push(feed);
+        });
+		
+		client.on('device-new', (device) => {
+            var feed = {
+                name: device.alias,
+                ip: device.host
+            };
+            array.push(feed);
+        });
+
+        function function1() {
+            console.log(array);
+            res.end(JSON.stringify(array));
+        }
+
+        setTimeout(function1, 250);
+		
+		function function2() {
+			array = [];
+        }
+
+        setTimeout(function2, 500);
+
+    } else if (cmd == "power") {
+
+        var temp = parseInt(req.query.ct, 10);
+        var hue = req.query.hue;
+        var st = req.query.st;
+        var bbrightness = parseInt(req.query.bri, 10);
+        var trans = parseInt(req.query.trans, 10);
+        var bip = req.query.ip;
+        if (!bip) {
+            rresponse = 'No IP Given';
+            res.end(JSON.stringify(rresponse));
+        } else {
+
+            if (st == "off") {
+
+                console.log('');
+                client.getDevice({
+                    host: bip
+                }).then(device => {
+                    device.lighting.setLightState({
+                        on_off: false,
+                    });
+                });
+                rresponse = "Bulb Turned Off";
+                res.end(JSON.stringify(rresponse));
+
+            } else if (st == "on") {
+
+                console.log('');
+                client.getDevice({host: bip}).then(device => {
+                    device.lighting.setLightState({
+                        on_off: true,
+                    });
+                });
+                rresponse = "Bulb Turned On";
+                res.end(JSON.stringify(rresponse));
+
+            } else {
+
+                if (!temp & !hue) {
+                    rresponse = 'No Colour Temp or Hex Given';
+                    res.end(JSON.stringify(rresponse));
+                } else {
+
+                    if (temp) {
+
+                        if (temp < 2500) {
+                            var ctmp = 2500;
+
+                        } else if (temp > 6500) {
+                            var ctmp = 6500;
+
+                        } else {
+                            var ctmp = temp;
+
+                        }
+
+                        if (bbrightness < 0) {
+                            var bbrightness = 1;
+
+                        } else if (bbrightness > 100) {
+                            var bbrightness = 100;
+
+                        } else if (isNaN(bbrightness)) {
+                            var bbrightness = 100;
+                        }
+
+                        if (trans == null) {
+                            var trans = 0;
+                        }
+
+                        var brightnum = Number(bbrightness);
+
+                        console.log('IP Given: ' + bip);
+                        console.log('Brightness: ' + brightnum);
+                        console.log('Transition in secs: ' + trans / 1000);
+                        console.log('Given Temp: ' + req.query.ct);
+                        console.log('Bulb Temp: ' + ctmp);
+                        console.log('');
+						client.getDevice({ host: bip }).then(device => {
+							device.lighting.setLightState({
+								on_off: true,
+								transition_period: trans,
+                                hue: 0,
+                                saturation: 0,
+                                brightness: brightnum,
+                                color_temp: ctmp
+							});;
+						});
+
+                        rresponse = "Bulb State Updated";
+                        res.end(JSON.stringify(rresponse));
+
+                    } else if (hue) {
+
+                        if (bbrightness < 0) {
+                            var bbrightness = 1;
+
+                        } else if (bbrightness > 100) {
+                            var bbrightness = 100;
+
+                        } else if (isNaN(bbrightness)) {
+                            var bbrightness = 100;
+                        }
+
+                        if (trans == null) {
+                            var trans = 0;
+                        }
+
+                        var brightnum = Number(bbrightness);
+
+                        hue = "#" + hue;
+
+                        var color = colorsys.hexToHsl(hue)
+
+                        console.log('IP Given: ' + bip);
+                        console.log('Hex: ' + hue);
+                        console.log('Brightness: ' + brightnum);
+                        console.log('Transition in secs: ' + trans / 1000);
+                        console.log('');
+						client.getDevice({ host: bip }).then(device => {
+							device.lighting.setLightState({
+								on_off: true,
+								transition_period: trans,
+                                hue: color.h,
+                                saturation: color.s,
+                                brightness: brightnum,
+                                color_temp: 0
+							});;
+						});
+
+                        rresponse = "Bulb State Updated";
+                        res.end(JSON.stringify(rresponse));
+
+                    }
+
+                }
+
+            }
+
+        }
+		console.log("Secondary Command Given: " + st);
+    } else if (cmd == "info") {
+
+        var bip = req.query.ip;
+        if (!bip) {
+            rresponse = 'No IP Given';
+            res.end(JSON.stringify(rresponse));
+        } else {
+			client.getDevice({ host: bip }).then(device => {
+				device.getSysInfo().then(info => {
+                    res.end(JSON.stringify(info));
+                })
+			});
+        }
+
+    } else if (cmd == "cinfo") {
+
+        var bip = req.query.ip;
+        if (!bip) {
+            rresponse = 'No IP Given';
+            res.end(JSON.stringify(rresponse));
+        } else {
+			client.getDevice({ host: bip }).then(device => {
+				device.cloud.getInfo().then(info => {
+                    res.end(JSON.stringify(info));
+                })
+			});
+        }
+
+    } else if (cmd == "schedule") {
+
+        var bip = req.query.ip;
+        if (!bip) {
+            rresponse = 'No IP Given';
+            res.end(JSON.stringify(rresponse));
+        } else {
+
+			client.getDevice({ host: bip }).then(device => {
+				device.schedule.getRules().then(info => {
+                    res.end(JSON.stringify(info));
+                })
+			});
+
+        }
+
+    } else {
+        rresponse = "No CMD Given";
+        res.end(JSON.stringify(rresponse));
+    }
+
+});
   
-  console.log(''); 
-  console.log("Command Given: " + cmd);
-  
-  if (cmd == "scan") {
-  
-  const scan = TPLSmartDevice.scan()
-    .on('light', light => {
-     console.log(light.host); 
-     rresponse = light.host;
-     res.end(JSON.stringify(rresponse));
-    })
-  
-  } else if (cmd == "power") {
-  
-  var temp = parseInt(req.query.ct, 10);
-  var hue = req.query.hue;
-  var st = req.query.st;
-  var bbrightness = parseInt(req.query.bri, 10);
-  var trans = parseInt(req.query.trans, 10);
-  var bip = req.query.ip;
-  if(!bip) {
-    rresponse = 'No IP Given';
-    res.end(JSON.stringify(rresponse));
-  } else {
-    
-  if (st == "off") {
-    
-  const bulb = new TPLSmartDevice(bip);
-  console.log('');
-    bulb.power(false)
-        .then(status => {
-          console.log(status)
-        })
-  rresponse = "Bulb Turned Off";  
-  res.end(JSON.stringify(rresponse));
-    
-  } else if (st == "on") {
-    
-  const bulb = new TPLSmartDevice(bip);
-  console.log('');
-    bulb.power(true)
-        .then(status => {
-          console.log(status)
-        })
-  rresponse = "Bulb Turned On";  
-  res.end(JSON.stringify(rresponse));
-    
-  } else {
-  
-  if (!temp & !hue) {
-    rresponse = 'No Colour Temp or Hex Given';
-    res.end(JSON.stringify(rresponse));
-  } else {
-    
-  if(temp) {
-  
-  if (temp < 2500) {
-  var	ctmp = 2500;
-  
-  } else if (temp > 6500){
-  var	ctmp = 6500;
-      
-  } else {
-  var ctmp = temp;
-    
-  }
-  
-  if (bbrightness < 0) {
-  var	bbrightness = 1;
-  
-  } else if (bbrightness > 100){
-  var	bbrightness = 100;
-      
-  } else if (isNaN(bbrightness)){
-  var	bbrightness = 100;	
-  }
-  
-  if (trans == null){
-  var	trans = 0;
-  }
-  
-  var brightnum = Number(bbrightness);
-  
-  console.log('IP Given: ' + bip);
-  console.log('Brightness: ' + brightnum);
-  console.log('Transition in secs: ' + trans / 1000);
-  console.log('Given Temp: ' + req.query.ct);
-  const bulb = new TPLSmartDevice(bip);
-  console.log('Bulb Temp: ' + ctmp);
-  console.log('');
-  bulb.power(true, trans, {hue: 0, saturation: 0, brightness: brightnum, color_temp: ctmp})
-  .then(status => {
-    console.log(status)
-  })
-  .catch(err => console.error(err))  
-  
-  rresponse = "Bulb State Updated";  
-  res.end(JSON.stringify(rresponse));
-  
-  } else if (hue) {
-    
-  if (bbrightness < 0) {
-  var	bbrightness = 1;
-  
-  } else if (bbrightness > 100){
-  var	bbrightness = 100;
-      
-  } else if (isNaN(bbrightness)){
-  var	bbrightness = 100;	
-  }
-  
-  if (trans == null){
-  var	trans = 0;
-  }
-  
-  var brightnum = Number(bbrightness);
-  
-  hue = "#" + hue;
-  
-  var color = colorsys.hexToHsl(hue)
-  
-  console.log('IP Given: ' + bip);
-  console.log('Hex: ' + hue);
-  console.log('Brightness: ' + brightnum);
-  console.log('Transition in secs: ' + trans / 1000);
-  console.log('');
-  
-  const bulb = new TPLSmartDevice(bip);
-  bulb.power(true, trans,{hue: color.h, saturation: color.s, brightness: brightnum, color_temp: 0})
-  .then(status => {
-    console.log(status)
-  })
-  .catch(err => console.error(err))  
-  
-  rresponse = "Bulb State Updated";  
-  res.end(JSON.stringify(rresponse));
-  
-  }
-  
-  }
-  
-  }
-  
-  }
-  
-  } else if (cmd == "info") {
-  
-  var bip = req.query.ip;
-  if(!bip) {
-    rresponse = 'No IP Given';
-    res.end(JSON.stringify(rresponse));
-  } else {
-  
-  const bulb = new TPLSmartDevice(bip);
-  bulb.info()
-    .then(info => {
-    res.end(JSON.stringify(info));
-    })
-  }
-  
-  } else if (cmd == "cinfo") {
-  
-  var bip = req.query.ip;
-  if(!bip) {
-    rresponse = 'No IP Given';
-    res.end(JSON.stringify(rresponse));
-  } else {
-  
-  const bulb = new TPLSmartDevice(bip);
-  bulb.cloud()
-    .then(info => {
-    res.end(JSON.stringify(info));
-    })
-  }
-  
-  } else if (cmd == "details") {
-  
-  var bip = req.query.ip;
-  if(!bip) {
-    rresponse = 'No IP Given';
-    res.end(JSON.stringify(rresponse));
-  } else {
-  
-  const bulb = new TPLSmartDevice(bip);
-  bulb.details()
-    .then(details => {
-    res.end(JSON.stringify(details));
-    })
-  }
-  
-  } else if (cmd == "schedule") {
-  
-  var bip = req.query.ip;
-  if(!bip) {
-    rresponse = 'No IP Given';
-    res.end(JSON.stringify(rresponse));
-  } else {
-  
-  const bulb = new TPLSmartDevice(bip);
-  bulb.schedule()
-    .then(schedule => {
-    res.end(JSON.stringify(schedule));
-    })
-  }
-  
-  } else {
-  rresponse = "No CMD Given";
-  res.end(JSON.stringify(rresponse));
-  }
-  
-  });
-  
-  appex.listen(interport);
-  console.log("Server listening on port " + interport);
+appex.listen(interport);
+console.log("Server listening on port " + interport);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
